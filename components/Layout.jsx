20259788bfb7f0b1cfbd87ce77da5e9e337cf6fa
Abhishek Upperwal <a.upperwal@gmail.com>
2023-08-13 14:31:53 +0530

    added title and description

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,3 +1,5 @@
+import Head from "next/head";
+
 import Footer from '@/components/Footer';
 import Header from '@/components/Header';
 
@@ -6,6 +8,10 @@ import styles from '@/styles/Layout.module.scss';
 export default function Layout({children}) {
     return (
         <section className={styles.layout_section}>
+            <Head>
+                <title>Advancing AI research towards ethical Artificial General Intelligence</title>
+                <meta name='description' content='Soket Labs is an AI research firm with a vision to further the advancement in AI towards ethical AGI. We are beginning this journey by build an indigenous large language model trained on Indic languages.'/>
+            </Head>
             <Header/>
             {children}
             <Footer/>
