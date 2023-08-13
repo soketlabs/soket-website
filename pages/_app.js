2904655424a75d047a5a7607c2e37ee9b9a0ea57
Abhishek Upperwal <a.upperwal@gmail.com>
2023-08-13 14:32:00 +0530

    added gAnalytics tracking

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,28 @@
+import Script from 'next/script';
+
 import Layout from '@/components/Layout';
 
 import '@/styles/globals.scss';
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-V1WQ63R32Y"/>
+      <Script
+      id='google-analytics'
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+        __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-V1WQ63R32Y');
+        `,
+        }}
+    />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   )
 }
