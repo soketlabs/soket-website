e20283ee09197ad519a853ba547ad3d13b6b4a66
Abhishek Upperwal <a.upperwal@gmail.com>
2023-08-15 12:34:15 +0530

    typo

--- pages/company.jsx
+++ pages/company.jsx
@@ -5,7 +5,7 @@ export default function Company() {
         <section className={styles.company_container}>
             {/* <h3 className={styles.topic_heading}>COMPANY</h3> */}
             {/* <h1>Soket Labs</h1> */}
-            <p><span className={styles.bold}>Soket Labs</span> is an AI research firm with a vision to <span className={styles.highlight}>further the advancement in AI towards ethical AGI.</span> We are beginning this journey by build an indigenous large language model trained on Indic languages.</p>
+            <p><span className={styles.bold}>Soket Labs</span> is an AI research firm with a vision to <span className={styles.highlight}>further the advancement in AI towards ethical AGI.</span> We are beginning this journey by building an indigenous large language model trained on Indic languages.</p>
         </section>
     )
 }
