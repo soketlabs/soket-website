745bbbf63b03b1f91f293edecf60950a373df663
Abhishek Upperwal <a.upperwal@gmail.com>
2025-04-18 01:06:50 +0530

    design changes

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -9,7 +9,7 @@ export default function Layout({children}) {
     return (
         <section className={styles.layout_section}>
             <Head>
-                <title>Advancing AI research towards ethical Artificial General Intelligence</title>
+                <title>Soket AI: Frontier AI from India</title>
                 <meta name='description' content='Soket Labs is an AI research firm with a vision to further the advancement in AI towards ethical AGI. We are beginning this journey by build an indigenous large language model trained on Indic languages.'/>
             </Head>
             <Header/>
